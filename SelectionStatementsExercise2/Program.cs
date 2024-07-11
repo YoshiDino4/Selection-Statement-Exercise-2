451584fc7d0e42275d732e358bd6b9893172b471
Yash Bajaj <yash@bajajmd.net>
2024-07-11 12:26:47 -0400

    Finished exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Wow! Look at you, mathematician!");
+                    break;
+                case "science":
+                    Console.WriteLine("Wow! Look at you, scientist!");
+                    break;
+                case "english":
+                    Console.WriteLine("Wow! Look at you, author!");
+                    break;
+                case "art":
+                    Console.WriteLine("Wow! Look at you, artist!");
+                    break;
+                case "history":
+                    Console.WriteLine("Wow! Look at you, historian!");
+                    break;
+                default:
+                    Console.WriteLine($"Wow! Good for you, {subject} sounds interesting!");
+                    break;
+                    
+            }
         }
     }
 }
